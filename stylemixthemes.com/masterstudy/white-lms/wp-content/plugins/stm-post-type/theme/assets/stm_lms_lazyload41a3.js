75fa54ee2a4d8d180af4bffa407d8c71f3ca1c8b
vrushalik789 <84567896+vrushalik789@users.noreply.github.com>
2021-12-13 16:02:53 +0530

    Delete stylemixthemes.com/masterstudy/white-lms directory

--- stylemixthemes.com/masterstudy/white-lms/wp-content/plugins/stm-post-type/theme/assets/stm_lms_lazyload41a3.js
+++
@@ -1,5 +0,0 @@
-(function ($) {
-    document.addEventListener('lazybeforeunveil', function (e) {
-        $(e.target).closest('.stm_lms_lazy_image').addClass('stm_lms_lazy_image__lazyloaded');
-    });
-})(jQuery);
